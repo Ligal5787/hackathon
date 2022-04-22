02bce9fe9432b06ca4683882c2c3dcff5b3f0833
Ligal5787 <101662583+Ligal5787@users.noreply.github.com>
2022-04-22 19:53:30 +0300

    Add files via upload

---
+++ Hackathon/script.js
@@ -0,0 +1,16 @@
+function loadWaldo() {
+    document.getElementById('Waldo').src = "http://assets.stickpng.com/thumbs/58b82c4bdcf6e61433da9496.png"
+  };
+  function WaldoMoves() {
+    var picture = document.getElementById('Waldo')
+  };
+  function WaldoMoves() {
+    alert('Woohoo, you win! You found Waldo!')
+    var picture = document.getElementById('Waldo')
+  
+    var x = Math.random() * 300
+    var y = Math.random() * 300
+    picture.style.top = x + 'px'
+  picture.style.left = y + 'px'
+  };
+
